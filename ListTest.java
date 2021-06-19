6bd7166f632ad12d250338b6b459c7bac296f69a
Dominick Nelson <60334994+nicknelson97@users.noreply.github.com>
2021-06-18 17:16:29 -0700

    Finished work
    Working

---
+++ ListTest.java
@@ -0,0 +1,25 @@
+// Class to test methods of LinkedIntList class
+
+public class ListTest {
+	
+    public static void main(String[] args) {
+     
+
+        LinkedIntList list = new LinkedIntList();
+        processList(list);
+    }
+
+    public static void processList(LinkedIntList list) {
+        list.add(20);
+        list.add(11);
+        list.add(73);
+        list.add(111);
+        System.out.println(list);
+        list.remove(1);
+        System.out.println(list);
+        System.out.print("is sorted? ");
+        System.out.print(list.isSorted(list));
+    }
+
+    
+}
